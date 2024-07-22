2eced078bf93dc26299fd296257ff824a4aba740
leandro-reguero <lreguerolazaro@gmail.com>
2024-07-22 12:58:10 +0200

    appy complete

--- src/app.py
+++ src/app.py
@@ -21,12 +21,14 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, BernoulliNB
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from pickle import dump
 import os
 from sqlalchemy import create_engine
 from sqlalchemy import text
 import pandas as pd
 from dotenv import load_dotenv
+import ast
 
 # reading the datasets and saving it
 movies = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/k-nearest-neighbors-project-tutorial/main/tmdb_5000_movies.csv')
@@ -81,3 +83,125 @@ df = merged_data[['movie_id', 'title', 'genres', 'overview', 'keywords', 'cast',
 print(df.head())
 df.to_csv('../data/processed/movies_mergedcleaned.csv', index=False)
 
+# Convert the genre column from string to list of dictionaries
+df['genres'] = df['genres'].apply(ast.literal_eval)
+df['keywords'] = df['keywords'].apply(ast.literal_eval)
+df['cast'] = df['cast'].apply(ast.literal_eval)
+
+# defining function to extract names from json format
+def extract_names(items):
+    return ', '.join([item['name'] for item in items])
+
+# applying name extraction funciton to the relevant columns
+df['genre_names'] = df['genres'].apply(extract_names)
+df.drop(columns=['genres'], inplace=True)
+df['keywords_names'] = df['keywords'].apply(extract_names)
+df.drop(columns=['keywords'], inplace=True)
+df['cast_names'] = df['cast'].apply(extract_names)
+df.drop(columns=['cast'], inplace=True)
+
+# renaming columns for better readability
+df.rename(columns={'genre_names': 'genres', 'keywords_names': 'keywords', 'cast_names': 'cast'}, inplace=True)
+
+# splitting the text in the columns:
+df['genres'] = df['genres'].apply(lambda x: x.split(', ') if isinstance(x, str) else [])
+df['keywords'] = df['keywords'].apply(lambda x: x.split(', ') if isinstance(x, str) else [])
+df['cast'] = df['cast'].apply(lambda x: x.split(', ') if isinstance(x, str) else [])
+
+# converting the columns crew (json) to list and overwiew (plain text) to list:
+def convert_to_list(item):
+    if isinstance(item, str):
+        try:
+            return ast.literal_eval(item)
+        except:
+            return []
+    elif item is None:
+        return []
+    else:
+        return item
+    
+df['crew'] = df['crew'].apply(convert_to_list)
+
+
+def text_to_list(text):
+    if isinstance(text, str):
+        return [text]
+    else:
+        return []
+
+df['overview'] = df['overview'].apply(text_to_list)
+
+# Extracting first three cast names:
+def extract_first_three(cast_list):
+    if len(cast_list) > 3:
+        return cast_list[:3]
+    else:
+        return cast_list
+    
+df['cast'] = df['cast'].apply(extract_first_three)
+
+# Extracting director's name:
+def extract_director(crew_list):
+    return [member['name'] for member in crew_list if member['job'] == 'Director']
+
+# storing it in a new column and deleting the original crew column:
+df['director'] = df['crew'].apply(extract_director)
+df = df.drop('crew', axis=1) 
+
+# Combine all columns into 'tags' column
+def combine_to_tags(row):
+    parts = [] # creating a list to store all pieces of text to be combined
+     # checking if the column is a list, and if so add it to 'parts'
+    if isinstance(row['overview'], list):
+        parts.extend(row['overview'])
+    if isinstance(row['genres'], list):
+        # for all other columns, remove spaces between words before adding them to 'parts'
+        row['genres'] = [i.replace(" ", "") for i in row['genres']]
+        parts.extend(row['genres'])
+    if isinstance(row['cast'], list):
+        row['cast'] = [i.replace(" ", "") for i in row['cast']]
+        parts.extend(row['cast'])
+    if isinstance(row['director'], list):
+        row['director'] = [i.replace(" ", "") for i in row['director']]
+        parts.extend(row['director'])
+    
+    return parts
+# creating the column tags and applying the function
+df['tags'] = df.apply(combine_to_tags, axis=1)
+
+# now joining all the elements of parts into a single string
+df["tags"] = df["tags"].apply(lambda x: ",".join(x).replace(",", " "))
+# removing the original columns:
+df.drop(columns = ["genres", "keywords", "cast", "director", "overview"], inplace = True)
+
+print(df['tags'][0]) # checking if it worked
+print(df.head())
+
+# saving the data
+df.to_csv('../data/processed/final_data.csv', index=False)
+
+# VECTORIZING THE TAGS
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(df['tags'])
+
+# APPLYING COSINE SIMILARITY (KNN) TO THE TAGS
+similarity = cosine_similarity(X)
+
+# DEFINING THE RECOMMENDER FUNCTION:
+new_df = df.copy()
+
+def recommend(movie):
+    # get the index of the movie we want to find similar movies to
+    movie_index = new_df[new_df["title"] == movie].index[0]
+    # finding the nearest cosine similar movies
+    distances = similarity[movie_index]
+    # create the recommendation list of movies
+    movie_list = sorted(list(enumerate(distances)), reverse = True , key = lambda x: x[1])[1:6]
+    
+    for i in movie_list:
+        print(new_df.iloc[i[0]].title)
+
+# asking the user for a movie to find recommendations
+film = str(input("Enter a movie to find recommendations:  "))
+film = film.lower()
+recommend(film)
